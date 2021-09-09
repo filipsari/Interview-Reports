6efda2cc46d7e07c4594619685707fd089865f5a
Darko Kovačević <darkokovacevic018@gmail.com>
2021-09-09 20:47:01 +0200

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
-import React, {Fragment  } from 'react';
-import './App.css';
-
-
-
-
-
-
-
-
-
+import React, { Fragment } from "react";
+import "./App.css";
 
 function App() {
-  return (
-
-
-    <h1> Big Four </h1>
-  );
+  return <h1> Big Four </h1>;
 }
 
 export default App;
