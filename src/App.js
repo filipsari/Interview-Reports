1fd1f5ea6b7327b585091174cc45c04c30e13c42
Darko Kovačević <darkokovacevic018@gmail.com>
2021-09-10 19:22:20 +0200

    create header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { Fragment } from "react";
+
+import { Header } from "./Components/Header/Header";
 import "./App.css";
 
 function App() {
-  return <h1> Big Four </h1>;
+  return (
+    <>
+      <Header />
+
+      {/* <h1> Big Four </h1>; */}
+    </>
+  );
 }
 
 export default App;
