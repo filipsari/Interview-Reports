521b1e989e7d8e955abb27d6ac3458a5262c555e
vladanpetrovic87 <vladanpetrovicvr@gmail.com>
2021-09-11 09:36:30 +0200

    app

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
-<<<<<<< HEAD
 import React, { Fragment } from "react";
 
 import { Header } from "./Components/Header/Header";
-=======
-import React from "react";
-import Footer from "./Components/Footer/Footer";
->>>>>>> 8112e90124b9896671fbe5270c6103072c4b194e
 import "./App.css";
 
 function App() {
   return (
-<<<<<<< HEAD
     <>
       <Header />
 
       {/* <h1> Big Four </h1>; */}
     </>
   );
-=======
-    <Footer />
-  )
-  
->>>>>>> 8112e90124b9896671fbe5270c6103072c4b194e
 }
 
 export default App;
