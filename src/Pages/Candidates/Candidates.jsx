624a52f18cc852a8d6093bf99f825fbb17ffbb3a
FilipSari <filipsari@gmail.com>
2021-09-14 19:23:14 +0200

    fixing bugs

--- src/Pages/Candidates/Candidates.jsx
+++ src/Pages/Candidates/Candidates.jsx
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+
 import { CandidateInfo } from "../../Components/SingleCandidateCard/CandidateInfo/CandidateInfo";
+import { getCandidates } from "../../Services/service";
+
 import "./Candidates.css";
 
-export const Candidates = ({ourData}) => {
+export const Candidates = () => {
+  const [candidates, setCandidates] = useState([]);
+  const [query, setQuery] = useState('');
 
 
-  return ( 
+  useEffect(() => {
+    getCandidates().then((candidates) => {
+      console.log(candidates);
+      setCandidates(candidates);
+    });
+  }, []);
+
+  const filteredCandidates = candidates.filter((c) => c.name === query);
+
+  return (
     <div className="card-group">
-      {ourData.map((element) => (
+      <input type="text" />
+      {candidates.map((element) => (
         <div className="card ourCard" style={{ width: "18rem" }}>
           <Link to="/single-candidate">
-            <img src="https://image.shutterstock.com/image-vector/profile-photo-vector-placeholder-pic-600w-535853263.jpg" className="card-img-top" alt="..." />
-             <CandidateInfo name={element.name} email={element.email}/>
+            <img
+              src="https://image.shutterstock.com/image-vector/profile-photo-vector-placeholder-pic-600w-535853263.jpg"
+              className="card-img-top"
+              alt="..."
+            />
+            <CandidateInfo name={element.name} email={element.email} />
           </Link>
         </div>
       ))}
--- src/Services/service.js
+++ src/Services/service.js
@@ -4,14 +4,13 @@ export const getCandidates = () => {
   const token= localStorage.getItem('accessToken');
   return fetch(candidatesEndPoint, {
     headers: {
-      authorization: `Bearer ${token}`
+      Authorization: `Bearer ${token}`
     }
   })
   .then( (results) => results.json())
   .then( (candidates) => {
     console.log(candidates);
     return candidates
-    
   })
 } 
 
